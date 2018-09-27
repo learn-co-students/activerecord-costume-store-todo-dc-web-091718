7bee21b3f0fcc80bf421b161e5901e681abbeac2
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-27 17:55:28 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,22 @@
 # Create your haunted_houses migration here
+
+
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      name
+location
+theme
+price
+whether they family friendly or not
+opening date
+closing date
+long description
+
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
