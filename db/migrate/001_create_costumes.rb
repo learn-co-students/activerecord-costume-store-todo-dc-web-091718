9737ccb0cd35de07031fd4757e7d2da436e3ce4d
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-28 17:42:49 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,13 +9,14 @@
 # value types according to the spec
 
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :image_url
       t.string :size
+      t.string :image_url
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,22 +1,18 @@
 # Create your haunted_houses migration here
 
 
-class CreateHauntedHouses < ActiveRecord::Migration
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      name
-location
-theme
-price
-whether they family friendly or not
-opening date
-closing date
-long description
-
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date
+      t.string :description
+      t.timestamps null: false
     end
   end
 end
